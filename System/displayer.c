f9262dbce37f877a2c6d9cd9722c18463ee965ee
luoqi <logeexpluoqi@outlook.com>
2021-04-13 15:54:03 +0800

    fix(displayer): delete some useless code

--- System/displayer.c
+++ System/displayer.c
@@ -2,38 +2,23 @@
  * @Author: luoqi 
  * @Date: 2020-10-14 22:28:08 
  * @Last Modified by: luoqi
- * @Last Modified time: 2020-11-10 11:15:38
+ * @Last Modified time: 2021-04-13 15:53:24
  */
 #include "displayer.h"
 #include "lcd12864.h"
 #include "fonts.h"
 
-Display displayer;
 
 void displayer_init()
 {
-    unsigned int i; 
-    
     lcd_init();
     displayer_clear();
 	lcd_bk_off_on(ON);
-
-    displayer.IsRefresh = 0;
-    for(i = 0; i < 10; i++)
-    {
-        displayer.DisplayLayer1[i] = 0;
-    }
 }
 
 void displayer_show()
 {
-    if(displayer.IsRefresh)
-    {
-        
-
-        
-        displayer.IsRefresh = 0;
-    }
+    
 }
 
 void displayer_clear()
